724e16a944fe09485f1dcf26f75fefc87825eeb1
PROPHESSOR <xenteze@gmail.com>
2018-08-29 10:59:09 +0300

    Генерация компилируемой, но не работающей карты
    Пока поддерживается только 1 сектор с прямоугольными стенами

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	gcc -g boom2doom3.c -o boom2doom3
+	gcc -g boom2doom3.c -o boom2doom3 -lm
 	./boom2doom3
 debug:
 	gdb boom2doom3
--- boom2doom3.c
+++ boom2doom3.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 // Patches
 
@@ -13,6 +14,9 @@
 // Constants
 
 #define SIDE_NULL 0xFFFF
+
+#define WALL_WEIGHT 8
+#define FLAT_WEIGHT 8
 
 // Structures
 
@@ -52,16 +56,24 @@ typedef struct {
 
 // Functions
 
-int getFileSize(void);
+int     getFileSize(void);
+int     getMinSectorFloor(int);
+int     getMaxSectorHeight(int);
+int16_t getLinedefAngle(int);
 
-bool parseVertexes(void);
-bool parseLinedefs(void);
-bool parseSidedefs(void);
-bool parseSectors(void);
+bool    parseVertexes(void);
+bool    parseLinedefs(void);
+bool    parseSidedefs(void);
+bool    parseSectors(void);
 
 // Global variables
 
 int fileSize = 0;
+
+uint16_t count_vertexes = 0;
+uint16_t count_linedefs = 0;
+uint16_t count_sidedefs = 0;
+uint16_t count_sectors  = 0;
 
 FILE        *file       = 0x0;
 vertex_t    *vertexes   = 0x0;
@@ -81,6 +93,46 @@ int getFileSize() {
     fseek(file, pos, SEEK_SET);
 
     return size;
+}
+
+int getMinSectorFloor(int linedefidx) {
+    linedef_t linedef = linedefs[linedefidx];
+    int min = sectors[sidedefs[linedef.s1].sector].height_floor;
+
+    if(linedefs[linedefidx].s2 == SIDE_NULL) return min;
+
+    int min2 = sectors[sidedefs[linedef.s2].sector].height_floor;
+
+    printf("getMinSectorFloor{%i}(%i, %i)", linedefidx, min, min2);
+
+    if(min2 < min) min = min2;
+
+    return min;
+}
+
+int getMaxSectorCeil(int linedefidx) {
+    linedef_t linedef = linedefs[linedefidx];
+    int max = sectors[sidedefs[linedef.s1].sector].height_ceil;
+
+    if(linedef.s2 == SIDE_NULL) return max;
+
+    int max2 = sectors[sidedefs[linedef.s2].sector].height_ceil;
+
+    printf("getMaxSectorCeil{%i}(%i, %i)", linedefidx, max, max2);
+
+    if(max2 > max) max = max2;
+
+    return max;
+}
+
+int16_t getLinedefAngle(int linedefidx) {
+    linedef_t linedef = linedefs[linedefidx]; // TODO: Проще передать указатель
+    vertex_t v1 = vertexes[linedef.v1];
+    vertex_t v2 = vertexes[linedef.v2];
+
+    int16_t angle = atan2(v2.y - v1.y, v2.x - v2.x);
+    
+    return angle; // ±1: |, 0: —
 }
 
 
@@ -103,6 +155,67 @@ int main() {
     if(!parseSectors()) return 1;
     printf(" ===> OK! <===\n\n");
 
+    printf("Generating DooM 3 map file...\n");
+    file = fopen("output/map.map", "w");
+        fprintf(file, "Version 2\n// entity 0\n");
+        fprintf(file, "{\n\"classname\" \"worldspawn\"\n");
+
+        for(uint16_t i = 0; i < count_linedefs; i++) {
+            fprintf(file, "{\n brushDef3\n {\n");
+            // TODO:
+            for(short j = 0; j < 6; j++) {
+                int16_t angle = getLinedefAngle(i);
+                int floor = getMinSectorFloor(i);
+                int ceil = getMaxSectorCeil(i);
+                bool isHorizontal = !angle;
+                bool isVertical   = !(abs(angle) - 1);
+                vertex_t v1 = vertexes[linedefs[i].v1];
+                vertex_t v2 = vertexes[linedefs[i].v2];
+
+
+                switch(j) { // View from top
+                    case 0: // Front
+                        fprintf(file, "  ( 0 0 -1 %i )", floor);
+                        break;
+                    case 1: // Right
+                        if(isHorizontal)    fprintf(file, "  ( 1 0 0 %i )", v1.x);//(v1.x + v2.x) / 2);
+                        else if(isVertical) fprintf(file, "  ( 1 0 0 %i )", WALL_WEIGHT);
+                        else printf("Custom angles not implemented!");
+                        break;
+                    case 2: // Back
+                        fprintf(file, "  ( 0 0 1 %i )", ceil);
+                        break;
+                    case 3: // Left
+                        if(isHorizontal)    fprintf(file, "  ( -1 0 0 %i )", v1.x);//(v1.x + v2.x) / 2);
+                        else if(isVertical) fprintf(file, "  ( -1 0 0 %i )", WALL_WEIGHT);
+                        else printf("Custom angles not implemented!");
+                        break;
+                    case 4: // Top
+                        if(isHorizontal) {
+                            fprintf(file, "  ( 0 -1 0 %i )", WALL_WEIGHT);
+                        } else if(isVertical) {
+                            fprintf(file, "  ( 0 -1 0 %i )", v1.x);//(v1.x + v2.x) / 2);
+                        } else printf("Custom angles not implemented!");
+                        break;
+                    case 5: // Bottom
+                        if(isHorizontal) {
+                            fprintf(file, "  ( 0 1 0 %i )", WALL_WEIGHT);
+                        } else if(isVertical) {
+                            fprintf(file, "  ( 0 1 0 %i )", v1.x);//(v1.x + v2.x) / 2);
+                        } else printf("Custom angles not implemented!");
+                        break;
+                    default:
+                        return 2;
+                }
+                fprintf(file, " ( ( 0.015625 0 0 ) ( 0 0.015625 0 ) ) \"textures/base_wall/lfwall27d\" 0 0 0\n");
+                fprintf(file, "// min: %i, max: %i, angle: %hi, vertical: %hi\n", getMinSectorFloor(i), getMaxSectorCeil(i), getLinedefAngle(i), isVertical);
+            }
+            fprintf(file, " }\n}\n");
+        }
+        fprintf(file, "}\n\n{\n\"classname\" \"info_player_start\"\n\"name\" \"info_player_start_1\"\n\"origin\" \"0 0 0\"\n\"angle\" \"180\"\n}");
+        fprintf(file, "\n{\n\"classname\" \"light\"\n\"name\" \"light1\"\n\"origin\" \"0 0 0\"\n\"_color\" \"0.78 0.78 0.84\"\n\"light_radius\" \"255 255 255\"\n}");
+    fclose(file);
+
     return 0;
 }
 
@@ -111,7 +224,7 @@ bool parseVertexes() {
     file = fopen("input/VERTEXES.lmp", "r");
     fileSize = getFileSize();
 
-    uint16_t count_vertexes = fileSize / sizeof(vertex_t) / 2;
+    count_vertexes = fileSize / sizeof(vertex_t) / 2;
     printf("\tFound: %i vertexes;\n", count_vertexes);
 
     vertexes = (vertex_t *)malloc(sizeof(vertex_t) * count_vertexes);
@@ -133,10 +246,10 @@ bool parseLinedefs() {
     file = fopen("input/LINEDEFS.lmp", "r");
     fileSize = getFileSize();
 
-    uint16_t count = fileSize / sizeof(linedef_t);
-    printf("\tFound: %i linedefs;\n", count);
+    count_linedefs = fileSize / sizeof(linedef_t);
+    printf("\tFound: %i linedefs;\n", count_linedefs);
 
-    linedefs = (linedef_t *)malloc(sizeof(linedef_t) * count);
+    linedefs = (linedef_t *)malloc(sizeof(linedef_t) * count_linedefs);
 
     for(uint16_t i = 0, idx = 0; i < fileSize; i += sizeof(linedef_t), idx++) {
         fread(&linedefs[idx], sizeof(linedefs[idx]), 1, file);
@@ -151,7 +264,7 @@ bool parseLinedefs() {
         );
     }
 
-    printf("\tLinedefs: %lu bytes;\n", sizeof(*linedefs) * count);
+    printf("\tLinedefs: %lu bytes;\n", sizeof(*linedefs) * count_linedefs);
 
     fclose(file);
 
@@ -163,10 +276,10 @@ bool parseSidedefs() {
     file = fopen("input/SIDEDEFS.lmp", "r");
     fileSize = getFileSize();
 
-    uint16_t count = fileSize / sizeof(sidedef_t);
-    printf("\tFound: %i sidedefs;\n", count);
+    count_sidedefs = fileSize / sizeof(sidedef_t);
+    printf("\tFound: %i sidedefs;\n", count_sidedefs);
 
-    sidedefs = (sidedef_t *)malloc(sizeof(sidedef_t) * count);
+    sidedefs = (sidedef_t *)malloc(sizeof(sidedef_t) * count_sidedefs);
 
     for(uint16_t i = 0, idx = 0; i < fileSize; i += sizeof(sidedef_t), idx++) {
         fread(&sidedefs[idx], sizeof(sidedefs[idx]), 1, file);
@@ -180,7 +293,7 @@ bool parseSidedefs() {
         );
     }
 
-    printf("\tSidedefs: %lu bytes\n", sizeof(*sidedefs) * count);
+    printf("\tSidedefs: %lu bytes\n", sizeof(*sidedefs) * count_sidedefs);
 
     fclose(file);
 
@@ -192,10 +305,10 @@ bool parseSectors() {
     file = fopen("input/SECTORS.lmp", "r");
     fileSize = getFileSize();
 
-    uint16_t count = fileSize / sizeof(sector_t);
-    printf("\tFound: %i sectors;\n", count);
+    count_sectors = fileSize / sizeof(sector_t);
+    printf("\tFound: %i sectors;\n", count_sectors);
 
-    sectors = (sector_t *)malloc(sizeof(sector_t) * count);
+    sectors = (sector_t *)malloc(sizeof(sector_t) * count_sectors);
 
     for(uint16_t i = 0, idx = 0; i < fileSize; i += sizeof(sector_t), idx++) {
         fread(&sectors[idx], sizeof(sectors[idx]), 1, file);
@@ -208,7 +321,7 @@ bool parseSectors() {
         );
     }
 
-    printf("Sectors: %lu bytes;\n", sizeof(*sectors) * count);
+    printf("Sectors: %lu bytes;\n", sizeof(*sectors) * count_sectors);
 
     fclose(file);
 
